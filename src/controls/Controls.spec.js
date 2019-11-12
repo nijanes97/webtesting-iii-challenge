3228971504244ad3aa32c599d022121277ff45f5
nijanes97 <nickjanes97@gmail.com>
2019-11-11 17:56:44 -0800

    finished mvp with tests for all display states and button states and attributes....working as intended

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,76 @@
+import React from 'react'
+import { render, fireEvent } from "@testing-library/react"
+import Controls from './Controls';
+
 // Test away!
+
+test('renders correctly', () => {
+    render(<Controls />);
+});
+
+test('lock gate button disabled when gate open', () => {
+    const { getByText } = render(
+        <Controls
+            closed={false}
+        />
+    )
+    expect(getByText(/lock gate/i)).toHaveProperty('disabled');
+})
+
+test('open gate button disabled when gate locked', () => {
+    const { getByText } = render(
+        <Controls
+            closed={true}
+            locked={true}
+        />
+    )
+    expect(getByText(/open gate/i)).toHaveProperty('disabled');
+})
+
+test('toggleClose called on button press', () => {
+    const setToggleClosed = jest.fn();
+    const { getByText } = render(
+        <Controls
+            toggleClosed={setToggleClosed}
+        />
+    )
+    fireEvent.click(getByText(/close gate/i));
+    expect(setToggleClosed).toHaveBeenCalled();
+})
+
+test('toggleClose called on button press', () => {
+    const setToggleClosed = jest.fn();
+    const { getByText } = render(
+        <Controls
+            closed={true}
+            toggleClosed={setToggleClosed}
+        />
+    )
+    fireEvent.click(getByText(/open gate/i));
+    expect(setToggleClosed).toHaveBeenCalled();
+})
+
+test('toggleLock called on button press', () => {
+    const setToggleLocked = jest.fn();
+    const { getByText } = render(
+        <Controls
+            closed={true}
+            toggleLocked={setToggleLocked}
+        />
+    )
+    fireEvent.click(getByText(/lock gate/i));
+    expect(setToggleLocked).toHaveBeenCalled();
+})
+
+test('toggleLock called on button press', () => {
+    const setToggleLocked = jest.fn();
+    const { getByText } = render(
+        <Controls
+            locked={true}
+            closed={true}
+            toggleLocked={setToggleLocked}
+        />
+    )
+    fireEvent.click(getByText(/unlock gate/i));
+    expect(setToggleLocked).toHaveBeenCalled();
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,44 @@
+import React from 'react'
+import { render } from "@testing-library/react"
+import Display from './Display';
+
 // Test away!
+
+test('renders correctly', () => {
+    render(<Display />);
+});
+
+test('open and unlocked displays with green led class', () => {
+    const { getByText } = render(
+        <Display />
+    )
+    getByText(/open/i);
+    getByText(/unlocked/i);
+    expect(getByText(/open/i).classList.contains('green-led')).toBe(true);
+    expect(getByText(/unlocked/i).classList.contains('green-led')).toBe(true);
+})
+
+test('lock gate button disabled when gate open', () => {
+    const { getByText } = render(
+        <Display 
+            closed={true}
+        />
+    )
+    getByText(/closed/i);
+    getByText(/unlocked/i);
+    expect(getByText(/closed/i).classList.contains('red-led')).toBe(true);
+    expect(getByText(/unlocked/i).classList.contains('green-led')).toBe(true);
+})
+
+test('lock gate button disabled when gate open', () => {
+    const { getByText } = render(
+        <Display 
+            closed={true}
+            locked={true}
+        />
+    )
+    getByText(/closed/i);
+    getByText(/locked/i);
+    expect(getByText(/closed/i).classList.contains('red-led')).toBe(true);
+    expect(getByText(/locked/i).classList.contains('red-led')).toBe(true);
+})
